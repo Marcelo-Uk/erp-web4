ceef8a579e5b8aa467c1e666ff270e6ca8f822d0
Marcelo-Uk <mribeirocorp@gmail.com>
2023-02-22 13:38:51 -0300

    Projeto Funcionando até antes dos menus e das novas páginas - Home Funcional e condicionais devidamente elaboradas

--- core/views.py
+++ core/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+import locale #to translate month name to Portuguese
+from datetime import datetime
 
 # Create your views here.
 
+@login_required
 def home(request):
-    return render(request,'core/home.html' )
-
-def pagina1(request):
-    return render(request,'core/pagina1.html')
-
-def pagina2(request):
-    return render(request,'core/pagina2.html')
+    if request.user.is_authenticated:
+        
+        locale.setlocale(locale.LC_TIME, 'pt_BR.utf-8')
+        data_hoje = datetime.now()
+        data = data_hoje.strftime('%d de %B de %Y ').capitalize()
+        context= {'data': data}
+        return render(request,'core/home.html', context )
+    else:
+        return render(request,'accounts/' )
--- leinertexweb4/urls.py
+++ leinertexweb4/urls.py
@@ -6,10 +6,8 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
     path('', views.home, name='home'),
-    path('pagina1/', views.pagina1, name='pagina1'),
-    path('pagina2/', views.pagina2, name='pagina2'),
+
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 
